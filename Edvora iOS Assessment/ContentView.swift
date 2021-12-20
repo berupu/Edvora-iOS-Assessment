8cfea5205cfaae2bb18412cf6972a98983cf685a
Rupu <rupuislamashraful7@gmail.com>
2021-12-20 15:58:30 +0600

    Initial Commit

---
+++ Edvora iOS Assessment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Edvora iOS Assessment
+//
+//  Created by be RUPU on 20/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Edvora iOS Assessment/Edvora_iOS_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edvora_iOS_AssessmentApp.swift
+//  Edvora iOS Assessment
+//
+//  Created by be RUPU on 20/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Edvora_iOS_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
